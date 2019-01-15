062e65ec046f78a08f0202f72818c0696c27a0cb
lufengren <35322784+lufengren@users.noreply.github.com>
2019-01-14 20:55:17 -0800

    core feature

--- src/app/share/delete-dialog/delete-dialog.component.ts
+++ src/app/share/delete-dialog/delete-dialog.component.ts
@@ -4,15 +4,27 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 @Component({
   selector: 'app-delete-dialog',
   template: `
-    <h3 mat-dialog-title>{{title}}</h3>
+  <div class="container">
+  <h3 mat-dialog-title>{{title}}</h3>
     <div mat-dialog-content>
       {{content}}
     </div>
-    <div mat-dialog-actions>
+    <div mat-dialog-actions class="button-container">
       <button mat-raised-button color="primary" (click)="onClick()">delete</button>
     </div>
+    </div>
   `,
-  styles: []
+  styles: [`
+  .button-container {
+    margin:1rem auto;
+  }
+  button {
+    background-color: rgba(187, 170, 164, 1.0);
+    border-radius: 5px;
+    font-size: 1rem;
+  }
+  `
+  ]
 })
 export class DeleteDialogComponent implements OnInit {
   title: '';
--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
+import { SearchComponent } from './search/search.component';
+
 import {
   MatToolbarModule,
   MatIconModule,
@@ -20,11 +22,40 @@ import {
   MatNativeDateModule,
   MatRadioModule,
   MatSelectModule,
-  MatSidenavModule
+  MatSidenavModule,
+  MatBadgeModule
 } from '@angular/material';
 
 
 @NgModule({
+  declarations: [DeleteDialogComponent, SearchComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatGridListModule,
+    MatDialogModule,
+    MatAutocompleteModule,
+    MatMenuModule,
+    MatCheckboxModule,
+    MatTooltipModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatBadgeModule
+  ],
+  entryComponents: [
+    DeleteDialogComponent
+  ],
   exports: [
     CommonModule,
     FormsModule,
@@ -46,11 +77,9 @@ import {
     MatNativeDateModule,
     MatRadioModule,
     MatSelectModule,
-    MatSidenavModule
-  ],
-  declarations: [DeleteDialogComponent],
-  entryComponents: [
-    DeleteDialogComponent
+    MatSidenavModule,
+    MatBadgeModule,
+    SearchComponent
   ]
 })
 export class ShareModule { }
